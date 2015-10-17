f6333c7bf048f8d287c1a77662feb89859ce972d
vbeakovic <valter.beakovic@gmail.com>
2015-10-17 18:24:34 +0200

    almost done...

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,23 @@
 ## By taking advantage of R's lexical scoping rules matrices that may require
 ## long or repeated computations are stored in a cached variable(list), allowing 
 ## for quick retrieval and computing only when changes occur.
+## IMPORTANT: the functions assume the matrix passed as parameter is invertible.
 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() as.matrix(x)
+        set_inverse <- function(inverse = matrix()) m <<- as.matrix(inverse)
+        get_inverse <- function() m
+        list(set = set, get = get,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 }
 
 
@@ -15,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$get_invers()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$set_inverse(m)
+        m
 }
