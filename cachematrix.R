f269c016f3db47e8d36929cbcc096de57894654c
vbeakovic <valter.beakovic@gmail.com>
2015-10-18 14:31:13 +0200

    varible names changed and few comments added

--- cachematrix.R
+++ cachematrix.R
@@ -11,16 +11,16 @@
 ## and provides a list of four functions to set and retreive the matrix and its
 ## inversion. Use x$function() to access each of the functions.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(xm = matrix()) {
         
-        m <- NULL
+        xim <- NULL #inverted matrix (xim)
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                xm <<- y #matrix (xm)
+                xim <<- NULL
         }
-        get <- function() as.matrix(x)
-        set_inverse <- function(inverse = matrix()) m <<- inverse
-        get_inverse <- function() m
+        get <- function() as.matrix(xm)
+        set_inverse <- function(inverse = matrix()) xim <<- inverse
+        get_inverse <- function() xim
         list(set = set, get = get,
              set_inverse = set_inverse,
              get_inverse = get_inverse)
@@ -32,15 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## return the inverted matrix ether by caluclating it (if it doesn't exist) or 
 ## by retreiving it form cache if it has been already calculated.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$get_invers()
-        if(!is.null(m)) {
+cacheSolve <- function(xm, ...) {
+
+        xim <- xm$get_invers() # inverted matrix (xim)
+        if(!is.null(xim)) {
                 message("getting cached data")
-                return(m)
+                return(xim)
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$set_inverse(m)
-        m
+        data <- xm$get() #matrix (xm)
+        xim <- solve(data, ...)
+        ## Return a matrix that is the inverse of 'm'
+        xm$set_inverse(xim)
+        xim
 }
